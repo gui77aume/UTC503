4cc734ff40f107994c2844fb243913e1790a1793
gui77aume <laisney.guillaume@gmail.com>
2020-04-08 15:49:51 +0200

    Ajout du pattern Observer pour notifier la fin du calcul

--- 03-Objet/fibonacci.ts
+++ 03-Objet/fibonacci.ts
@@ -1,3 +1,22 @@
+
+
+ class fibObserver{
+     private nom:string;
+     constructor(nomObservateur:string){
+         this.nom=nomObservateur;   
+    }
+     update(message:string):void{
+        console.log("Changement notifié à:" + this.nom + " : "+message);
+    }
+}
+
+interface Observable{
+    attach(observateur):void;
+    //detach():void;
+    //getState():boolean;
+    notifyChange(message:string):void;
+}
+
 interface Iterateur<T>{
     first():void;
     next():void;
@@ -5,36 +24,51 @@ interface Iterateur<T>{
     currentItem(): T;
 }
 
-class Fibonacci implements Iterateur<number> {
+class Fibonacci   implements Iterateur<number>,Observable {
     // fibonacci(n) = fibonnacci(n-1) + fibonnacci(n-2)
 
     private nMax : number;
     private index : number;
     private nMoinsUn : number;
     private nMoinsDeux : number
+    private observateurs : fibObserver[]; //TODO: on devrait avoir une liste d'observateurs, pas un seul
+    private nombreObservateurs;
    
-
     constructor (n:number){
         this.nMax=n;
         this.index=0;
         this.nMoinsDeux=-1;
         this.nMoinsUn=1;
-    
+        this.observateurs=new Array(10);
+        this.nombreObservateurs=0;
+    }
+
+    attach(o:fibObserver){
+        this.observateurs[this.nombreObservateurs]=o;
+        this.nombreObservateurs++;
+    }
+
+    notifyChange(message:string):void{
+        var i:number;
+            for (i=0;i<this.nombreObservateurs;i++) {
+                this.observateurs[i].update(message);
+            }
     }
 
     first():void{
         this.index=0;
         this.nMoinsDeux=0;
         this.nMoinsUn=1;
+        //this.notifyChange("First !");
     }
-
     
     next():void{
         var passage=this.nMoinsUn + this.nMoinsDeux;
         this.nMoinsDeux=this.nMoinsUn;
         this.nMoinsUn=passage;   
         this.index++;
-
+        if (this.isDone()){ this.notifyChange("Fin ! \n"); }
+        
     }
 
     isDone():boolean  {
@@ -61,7 +95,12 @@ class Fibonacci implements Iterateur<number> {
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34,
 // Fin (*)
 var f: Fibonacci ;
+var o1,o2 : fibObserver;
 f = new Fibonacci(10); // (**)
+o1 = new fibObserver("observateur n°1");
+f.attach(o1);
+o2 = new fibObserver("observateur n°2");
+f.attach(o2);
 while(!f.isDone()){
     console.log(`${f.currentItem()},`);
     f.next();
