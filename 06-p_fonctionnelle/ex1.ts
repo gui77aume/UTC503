d603af2070ae334af44bb017b05abd290eed8ea0
gui77aume <laisney.guillaume@gmail.com>
2020-04-15 22:13:35 +0200

    changement des noms de fonction en pour correspondre à la consigne ex3. Trop cool.

--- 06-p_fonctionnelle/ex1.ts
+++ 06-p_fonctionnelle/ex1.ts
@@ -1,10 +1,12 @@
-let generale= max => v=> v>max ? max : v;
-let particuliere3=generale(3);
-let particuliere4=generale(4);
+let maximize= max => v=> v>max ? max : v;
+let maximize3=maximize(3);
+let maximize4=maximize(4);
 
 
-console.log("retour = ",particuliere3(1));
-console.log("retour = ",particuliere3(6));
+console.log("retour = ",maximize3(1));
+console.log("retour = ",maximize3(6));
 
-console.log("retour = ",particuliere4(1));
-console.log("retour = ",particuliere4(6))
+console.log("retour = ",maximize4(1));
+console.log("retour = ",maximize4(6));
+
+
